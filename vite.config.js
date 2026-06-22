44515a9075732bd6b796bfa6e31786da3f079e19
WIN-WINNER <176belife@gmail.com>
2026-06-23 00:00:00 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    allowedHosts: ['5173-itmdg4azgop65lve32eu8-9ff3e630.sg1.manus.computer'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path
+      }
+    }
+  }
+})
